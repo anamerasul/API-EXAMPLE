a695ca7d731bf8a1f3cfc1759ecce2a1607a40ae
anamerasul <anamerasul@gmail.com>
2022-02-24 18:57:06 +0600

    Display dynamic search result using bootstrap cards

--- assets/js/mealdb.js
+++ assets/js/mealdb.js
@@ -11,5 +11,34 @@ const searchFood=()=>{
 
         fetch(url)
         .then(res=>res.json())
-        .then(data=>console.log(data))
+        .then(data=>displaySearchResult(data.meals))
+
+        
+}
+
+const displaySearchResult=meals=>{
+        console.log(meals);
+
+        const searchResult=document.getElementById('search-result')
+
+        meals.forEach(meal=>{
+                console.log(meal)
+                const div=document.createElement('div');
+
+                div.classList.add('col')
+
+                div.innerHTML=`  <div class="card h-100">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${meal.strMeal}</h5>
+                  <p class="card-text">${meal.strInstructions.slice(0,150)}</p>
+                </div>
+              </div>`;
+
+              searchResult.appendChild(div);
+        })
+
+        // for(const meal of meals){
+        //         console.log(meal[0])
+        // }
 }
